12fac002db265cbe17b9ac6f3d6ba41fbfea91ce
AASHISH <aashishkumar10122000@gmail.com>
2021-07-15 20:24:06 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_intro/pages/home_page.dart';
+import 'package:flutter_intro/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,15 @@ class MyApp extends StatelessWidget {
     //we use "MaterialApp" because we cannot return the container for launching the app
     //we have to return the app
 
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      //home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage()
+      },
+    );
   }
 }
